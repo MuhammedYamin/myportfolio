f46cfb95934163a4e183a43a4f15a7e886b1d83b
MuhammedYamin <muhammedyamin003@gmail.com>
2025-11-13 20:32:49 +0530

    update credentials

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 let withMDX;
 try {
-  // Ensure both @next/mdx and @mdx-js/loader are resolvable before attempting to require.
-  // @next/mdx will internally require @mdx-js/loader, which can cause a hard crash if missing.
+ 
   require.resolve('@next/mdx');
   require.resolve('@mdx-js/loader');
-  // safe to require
+ 
   withMDX = require('@next/mdx')({ extension: /\.mdx?$/ });
 } catch (err) {
-  // fallback - no-op passthrough if MDX support isn't fully installed yet
-  // This prevents startup errors; you can add MDX later by installing the packages.
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   withMDX = (cfg) => cfg;
 }
 
